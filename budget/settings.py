a95a80feb49b0f3eea6ff25e820d4d715581d691
ram-25082005 <rampalla2005@gmail.com>
2025-02-25 21:42:40 +0530

    ..

--- budget/settings.py
+++ budget/settings.py
@@ -33,7 +33,7 @@ if ENVIRONMENT=='development':
 else:
     DEBUG=True
 
-ALLOWED_HOSTS = ['budgetplanning-production.up.railway.app','localhost']
+#ALLOWED_HOSTS = ['budgetplanning-production.up.railway.app','localhost']
 ALLOWED_ORIGINS=['*']
 
 # Application definition
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'budget.urls'
-env("CSRF_COOKIE_SECURE")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,7 +82,7 @@ TEMPLATES = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://budgetplanning-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://budgetplanning-production.up.railway.app/*']
 
 
 WSGI_APPLICATION = 'budget.wsgi.application'
